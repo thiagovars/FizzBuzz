c1b28afae7215ed1536621d034ea76f197a5cca8
Thiago Vargas <thiagovars@gmail.com>
2015-02-26 20:48:07 -0300

    rebuilding do projeto.

--- Test/FizzBuzzPrinterTest.java
+++ Test/FizzBuzzPrinterTest.java
@@ -1,20 +1,13 @@
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class FizzBuzzPrinterTest {
+
     @Test
     public void retornaUmQuandoRecebeUm(){
         FizzBuzzPrinter printado = new FizzBuzzPrinter();
         String numeroImpresso = printado.shout(1);
         assertEquals("1", numeroImpresso);
-
     }
-
-    @Test
-    public void retornaUmQuandoRecebeUm(){
-        FizzBuzzPrinter dois = new FizzBuzzPrinter();
-        String 
-    }
-
 }
--- src/main.java
+++ src/main.java
@@ -1,18 +1,9 @@
 
 public class main {
     static public void main(String args[]) {
-        int i;
-        i = 10;
-        System.out.println("Hello Java." + i);
-
-        for (i=1; i < 101; i++) {
-            if(i%3 == 0) {
-                System.out.printf("Fizz");
-            } else if(i%5 == 0) {
-                System.out.printf("Buzz");
-            } else {
-                System.out.println("\n"+i);
-            }
+        FizzBuzzPrinter Billy = new FizzBuzzPrinter();
+        for(int i=1; i<101;i++) {
+            System.out.println(Billy.shout(i));
         }
     }
 }
